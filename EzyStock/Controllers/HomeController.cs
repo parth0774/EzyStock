e3866568f55141cc005dde2224da552d4d47f141
Bhagyesh Patel <patelbhagyesh12072003@gmail.com>
2023-12-03 23:26:45 -0500

    changed home controller

--- EzyStock/Controllers/HomeController.cs
+++ EzyStock/Controllers/HomeController.cs
@@ -1,29 +1,67 @@
-﻿using EzyStock.Models;
+﻿using EzyStock.DataAccess;
+using EzyStock.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EzyStock.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly DB_Context _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(DB_Context context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginInfo userlogin)
+        {
+            List<LoginInfo> login = _context.Logins.ToList();
+            LoginInfo user = _context.Logins.Where(u => u.UserName == userlogin.UserName && u.Password == userlogin.Password).FirstOrDefault();
+            if (user!=null)
+            {
+                if(user.UserName=="admin" && user.Password=="admin")
+                {
+                    return View("CreateAccounts");
+                }
+                else if (user.AccType==AccountType.Supplier)
+                {
+                    return RedirectToAction("GetSupplier","Suppliers");
+                }
+                else if (user.AccType==AccountType.User)
+                {
+                    return RedirectToAction("Index", "Inventory");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View("InvalidLogin");
+            }
+
+
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
