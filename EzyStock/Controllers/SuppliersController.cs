8820a8b29bed05af3a47719f4f3371d5f5ee68a2
Bhagyesh Patel <patelbhagyesh12072003@gmail.com>
2023-12-03 23:09:04 -0500

    Added supplier controller and views

---
+++ EzyStock/Controllers/SuppliersController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EzyStock.DataAccess;
+using EzyStock.Models;
+
+namespace EzyStock.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly DB_Context _context;
+
+        public SuppliersController(DB_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Suppliers
+        public async Task<IActionResult> Index()
+        {
+            return _context.Suppliers != null ?
+                        View(await _context.Suppliers.ToListAsync()) :
+                        Problem("Entity set 'DB_Context.Suppliers'  is null.");
+        }
+
+        // GET: Suppliers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Suppliers == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Suppliers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SupplierName,SupplierEmail,SupplierPhone")] Supplier supplier,string username,string password)
+        {
+            if (ModelState.IsValid)
+            {
+                LoginInfo loginInfo=new LoginInfo();
+                loginInfo.UserName = username;
+                loginInfo.Password = password;
+                loginInfo.AccType=AccountType.Supplier;
+                _context.Add(loginInfo);
+                _context.Add(supplier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Suppliers == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SupplierName,SupplierEmail,SupplierPhone")] Supplier supplier)
+        {
+            if (id != supplier.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supplier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupplierExists(supplier.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Suppliers == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Suppliers == null)
+            {
+                return Problem("Entity set 'DB_Context.Suppliers'  is null.");
+            }
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier != null)
+            {
+                _context.Suppliers.Remove(supplier);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupplierExists(int id)
+        {
+            return (_context.Suppliers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        // GET: Supplier
+        public  IActionResult SupplierOperation(int id)
+        {
+            var supplier =  _context.Suppliers.Find(id);
+            return View(supplier);
+        }
+        public  IActionResult GetSupplier()
+        {
+            var suppliers =  _context.Suppliers.ToList();
+            return View(suppliers);
+        }
+
+
+
+    }
+}
