a47d69d73ee2a2961523f7f471cb089f358de8a4
demellab <demellab@sheridancollege.ca>
2023-12-03 23:01:58 -0500

    Added inventory model

---
+++ EzyStock/Models/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EzyStock.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public DateOnly LastOrderDate { get; set; }
+        public int LastOrderQuantity { get; set; }
+        [ForeignKey("Product")]
+        public int ProductId { get; set; } 
+        public virtual Product? Product { get; set; }
+
+        //low  inventory intecator in view section 
+
+    }
+}
